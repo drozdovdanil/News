b01484312c574a66acc0fac2da0d344b6a1b4264
drozdovdanil <124682617+drozdovdanil@users.noreply.github.com>
2023-06-08 21:21:51 +0300

    Add files via upload

--- NewsPaper/news/models.py
+++ NewsPaper/news/models.py
@@ -45,6 +45,8 @@ class Post(models.Model):
     def preview(self):
         return self.text_news[0:125] + '...'
 
+    def __str__(self):
+        return f'{self.text_news.title()}'
 
 class PostCategory(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE)
--- NewsPaper/news/views.py
+++ NewsPaper/news/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.views.generic import ListView, DetailView
+from .models import Post
 
-# Create your views here.
+
+class ProductsList(ListView):
+    model = Post
+    ordering = '-text_news'
+    template_name = 'post.html'
+    context_object_name = 'post'
+
+
+class ProductDetail(DetailView):
+    # Модель всё та же, но мы хотим получать информацию по отдельному товару
+    model = Post
+    # Используем другой шаблон — product.html
+    template_name = 'pos.html'
+    # Название объекта, в котором будет выбранный пользователем продукт
+    context_object_name = 'post'

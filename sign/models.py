c92ed44850d97be9a00d9aa8796d11e462aff71a
drozdovdanil <124682617+drozdovdanil@users.noreply.github.com>
2023-06-29 18:07:28 +0300

    Add files via upload

---
+++ sign/models.py
@@ -0,0 +1,40 @@
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.models import User
+from django import forms
+from allauth.account.forms import SignupForm
+from django.contrib.auth.models import Group
+from django.core.mail import EmailMultiAlternatives
+
+from NewsPaper import settings
+
+
+class BaseRegisterForm(UserCreationForm):
+    email = forms.EmailField(label = "Email")
+    first_name = forms.CharField(label = "Имя")
+    last_name = forms.CharField(label = "Фамилия")
+
+    class Meta:
+        model = User
+        fields = ("username",
+                  "first_name",
+                  "last_name",
+                  "email",
+                  "password1",
+                  "password2", )
+
+
+class BasicSignupForm(SignupForm):
+
+    def save(self, request):
+        user = super(BasicSignupForm, self).save(request)
+        basic_group = Group.objects.get(name='common')
+        basic_group.user_set.add(user)
+        msg = EmailMultiAlternatives(
+            subject="Добро рожаловать",
+            body="Добро пожаловать",
+            from_email=settings.DEFAULT_FROM_EMAIL,
+            to=[user.email],
+        )
+
+        msg.send()
+        return user

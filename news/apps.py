b718b18df24a8a66d588ffc3d54a399e81aac936
drozdovdanil <124682617+drozdovdanil@users.noreply.github.com>
2023-06-29 18:10:11 +0300

    Delete news directory

--- news/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class NewsConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'news'
-
-    def ready(self):
-        from .signals import send_notification, notify_about_new_post
